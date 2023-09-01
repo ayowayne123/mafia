998eed4beb7b38422fab06c93bf6c44191f44ed6
ayowayne123 <ayowayne123@gmail.com>
2023-09-01 16:58:40 +0300

    added prisma geenrate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 // const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 // export const prisma =
 //   globalForPrisma.prisma ||
+
 //   new PrismaClient({
 //     log: ["query"],
 //   });
